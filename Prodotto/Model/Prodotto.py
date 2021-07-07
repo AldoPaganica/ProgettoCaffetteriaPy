f8a36ef3b9dc048dd012dcb20bbcf874ec3031b0
IsabellaMarini <76102845+IsabellaMarini@users.noreply.github.com>
2021-07-07 11:40:40 +0200

    first commit

---
+++ Prodotto/Model/Prodotto.py
@@ -0,0 +1,25 @@
+class Prodotto():
+    def __init__(self, nome, descrizione, immagine, valori_nutrizionali, lista_ingredienti,
+                 categoria, sottocategoria, prezzo, personalizzazione):
+        super(Prodotto, self).__init__()
+        self.nome=nome
+        self.descrizione=descrizione
+        self.immagine=immagine
+        self.valori_nutrizionali=valori_nutrizionali
+        self.lista_ingredienti=lista_ingredienti
+        self.categoria=categoria
+        self.sottocategoria=sottocategoria
+        self.prezzo=prezzo
+        self.personalizzazione=personalizzazione
+
+
+
+
+
+
+
+
+
+
+
+
